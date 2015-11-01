843ab00cf7dd0e8d15dd7645b191aa5f474f6f4d
unknown <njseeber@gmail.com>
2015-10-31 23:06:19 -0600

    Initial Commit

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace NicksTechTips.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
